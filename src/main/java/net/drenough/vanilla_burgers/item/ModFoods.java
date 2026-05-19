8eb047e98c262a6b64ad6c2b77b7d5d6c86b7cdb
dendy <dendy412@gmail.com>
2026-05-20 00:41:08 +0200

    Final rebalancing of food values

--- src/main/java/net/drenough/vanilla_burgers/item/ModFoods.java
+++ src/main/java/net/drenough/vanilla_burgers/item/ModFoods.java
@@ -4,37 +4,37 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
     public static final FoodProperties STEAK_BURGER = new FoodProperties.Builder()
-            .nutrition(10)
-            .saturationModifier(1.2F)
+            .nutrition(14)
+            .saturationModifier(1.4F)
             .build();
 
     public static final FoodProperties PORK_BURGER = new FoodProperties.Builder()
-            .nutrition(9)
-            .saturationModifier(1.1F)
-            .build();
-
-    public static final FoodProperties CHICKEN_BURGER = new FoodProperties.Builder()
-            .nutrition(7)
-            .saturationModifier(0.9F)
-            .build();
-
-    public static final FoodProperties RABBIT_BURGER = new FoodProperties.Builder()
-            .nutrition(6)
-            .saturationModifier(0.8F)
-            .build();
-
-    public static final FoodProperties COD_BURGER = new FoodProperties.Builder()
-            .nutrition(6)
-            .saturationModifier(0.7F)
-            .build();
-
-    public static final FoodProperties SALMON_BURGER = new FoodProperties.Builder()
-            .nutrition(7)
-            .saturationModifier(0.9F)
+            .nutrition(14)
+            .saturationModifier(1.4F)
             .build();
 
     public static final FoodProperties MUTTON_BURGER = new FoodProperties.Builder()
-            .nutrition(9)
-            .saturationModifier(1.0F)
+            .nutrition(13)
+            .saturationModifier(1.2F)
+            .build();
+
+    public static final FoodProperties CHICKEN_BURGER = new FoodProperties.Builder()
+            .nutrition(13)
+            .saturationModifier(1.2F)
+            .build();
+
+    public static final FoodProperties SALMON_BURGER = new FoodProperties.Builder()
+            .nutrition(13)
+            .saturationModifier(1.2F)
+            .build();
+
+    public static final FoodProperties COD_BURGER = new FoodProperties.Builder()
+            .nutrition(12)
+            .saturationModifier(1.1F)
+            .build();
+
+    public static final FoodProperties RABBIT_BURGER = new FoodProperties.Builder()
+            .nutrition(12)
+            .saturationModifier(1.1F)
             .build();
 }
